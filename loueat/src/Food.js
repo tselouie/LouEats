9d18efd4f893d20e2571d6907686803e6a62dc6c
tselouie <teslouie@gmail.com>
2018-10-28 23:23:56 -0400

    naming tweaks

--- loueat/src/Food.js
+++ loueat/src/Food.js
@@ -31,32 +31,32 @@ class Food extends Component {
   <div className="row">
     <div className="col-sm">
 <div className="card-container" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
-{this.state.isHovering && <div className="back"><h3>Ramen</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id enim nec dolor cursus posuere at non dolor. Sed eget tellus non dolor viverra congue non non velit. Duis fermentum viverra erat, ut dapibus magna porttitor ac. Vivamus turpis lorem, pharetra ac elit nec, convallis aliquam lectus. Suspendisse consequat, ligula at fermentum egestas, massa est pharetra ante, a posuere dolor mauris id nibh. Proin a est dictum, posuere dui nec, venenatis magna. Quisque ut malesuada magna. Vivamus ac turpis nec justo ultricies tristique id nec nisl.</p></div>}
-  <a href="/noodles" >
+{this.state.isHovering && <a href="/noodles" ><div className="back"><h3>Ramen</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id enim nec dolor cursus posuere at non dolor. Sed eget tellus non dolor viverra congue non non velit. Duis fermentum viverra erat, ut dapibus magna porttitor ac. Vivamus turpis lorem, pharetra ac elit nec, convallis aliquam lectus. Suspendisse consequat, ligula at fermentum egestas, massa est pharetra ante, a posuere dolor mauris id nibh. Proin a est dictum, posuere dui nec, venenatis magna. Quisque ut malesuada magna. Vivamus ac turpis nec justo ultricies tristique id nec nisl.</p></div></a>}
+  
     <img src="https://loremflickr.com/320/360/noodles"  alt="noodlepic" class="img-fluid"/>
-  </a>
+
   </div>
     </div>
 
        <div className="col-sm">
 <div className="card-container" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
-{this.state.isHovering && <div className="back"><h3>Exotic Foods</h3>
-</div>}
-  <a href="/noodles" >
-    <img src="https://loremflickr.com/320/360/breakfast"  alt="exoticfood" class="img-fluid"/>
-  </a>
+{this.state.isHovering && <a href="/spicy" ><div className="back"><h3>Exotic Foods</h3>
+</div></a>}
+  
+    <img src="https://loremflickr.com/320/360/spicy"  alt="exoticfood" class="img-fluid"/>
+  
   </div>
     </div>
     
 
     <div className="col-sm">
 <div className="card-container" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
-{this.state.isHovering && <div className="back"><h3>Ramen</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id enim nec dolor cursus posuere at non dolor. Sed eget tellus non dolor viverra congue non non velit. Duis fermentum viverra erat, ut dapibus magna porttitor ac. Vivamus turpis lorem, pharetra ac elit nec, convallis aliquam lectus. Suspendisse consequat, ligula at fermentum egestas, massa est pharetra ante, a posuere dolor mauris id nibh. Proin a est dictum, posuere dui nec, venenatis magna. Quisque ut malesuada magna. Vivamus ac turpis nec justo ultricies tristique id nec nisl.</p></div>}
-  <a href="/noodles" >
+{this.state.isHovering && <a href="/drinks" ><div className="back"><h3>Drinks</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id enim nec dolor cursus posuere at non dolor. Sed eget tellus non dolor viverra congue non non velit. Duis fermentum viverra erat, ut dapibus magna porttitor ac. Vivamus turpis lorem, pharetra ac elit nec, convallis aliquam lectus. Suspendisse consequat, ligula at fermentum egestas, massa est pharetra ante, a posuere dolor mauris id nibh. Proin a est dictum, posuere dui nec, venenatis magna. Quisque ut malesuada magna. Vivamus ac turpis nec justo ultricies tristique id nec nisl.</p></div></a>}
+  
     <img src="https://loremflickr.com/320/360/drinks"  alt="drinkspic" class="img-fluid"/>
-  </a>
+
   </div>
     </div>
   </div>
@@ -64,32 +64,31 @@ class Food extends Component {
  <div className="row" id="menurow2">
     <div className="col-sm">
 <div className="card-container" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
-{this.state.isHovering && <div className="back"><h3>Ramen</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id enim nec dolor cursus posuere at non dolor. Sed eget tellus non dolor viverra congue non non velit. Duis fermentum viverra erat, ut dapibus magna porttitor ac. Vivamus turpis lorem, pharetra ac elit nec, convallis aliquam lectus. Suspendisse consequat, ligula at fermentum egestas, massa est pharetra ante, a posuere dolor mauris id nibh. Proin a est dictum, posuere dui nec, venenatis magna. Quisque ut malesuada magna. Vivamus ac turpis nec justo ultricies tristique id nec nisl.</p></div>}
-  <a href="/noodles" >
+{this.state.isHovering && <a href="/desserts" ><div className="back"><h3>Dessert</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id enim nec dolor cursus posuere at non dolor. Sed eget tellus non dolor viverra congue non non velit. Duis fermentum viverra erat, ut dapibus magna porttitor ac. Vivamus turpis lorem, pharetra ac elit nec, convallis aliquam lectus. Suspendisse consequat, ligula at fermentum egestas, massa est pharetra ante, a posuere dolor mauris id nibh. Proin a est dictum, posuere dui nec, venenatis magna. Quisque ut malesuada magna. Vivamus ac turpis nec justo ultricies tristique id nec nisl.</p></div></a>}
+  
     <img src="https://loremflickr.com/320/360/dessert"  alt="dessertspic" class="img-fluid"/>
-  </a>
   </div>
     </div>
 
        <div className="col-sm">
 <div className="card-container" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
-{this.state.isHovering && <div className="back"><h3>Exotic Foods</h3>
-</div>}
-  <a href="/noodles" >
+{this.state.isHovering && <a href="/meat" ><div className="back"><h3>Meat</h3>
+</div></a>}
+
     <img src="https://loremflickr.com/320/360/meat"  alt="meatpic" class="img-fluid"/>
-  </a>
+ 
   </div>
     </div>
     
 
     <div className="col-sm">
 <div className="card-container" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
-{this.state.isHovering && <div className="back"><h3>Ramen</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id enim nec dolor cursus posuere at non dolor. Sed eget tellus non dolor viverra congue non non velit. Duis fermentum viverra erat, ut dapibus magna porttitor ac. Vivamus turpis lorem, pharetra ac elit nec, convallis aliquam lectus. Suspendisse consequat, ligula at fermentum egestas, massa est pharetra ante, a posuere dolor mauris id nibh. Proin a est dictum, posuere dui nec, venenatis magna. Quisque ut malesuada magna. Vivamus ac turpis nec justo ultricies tristique id nec nisl.</p></div>}
-  <a href="/noodles" >
+{this.state.isHovering && <a href="/snacks" ><div className="back"><h3>Snacks</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id enim nec dolor cursus posuere at non dolor. Sed eget tellus non dolor viverra congue non non velit. Duis fermentum viverra erat, ut dapibus magna porttitor ac. Vivamus turpis lorem, pharetra ac elit nec, convallis aliquam lectus. Suspendisse consequat, ligula at fermentum egestas, massa est pharetra ante, a posuere dolor mauris id nibh. Proin a est dictum, posuere dui nec, venenatis magna. Quisque ut malesuada magna. Vivamus ac turpis nec justo ultricies tristique id nec nisl.</p></div></a>}
+ 
     <img src="https://loremflickr.com/320/360/snacks"  alt="snack" class="img-fluid"/>
-  </a>
+
   </div>
     </div>
   </div>
--- loueat/src/Travel.js
+++ loueat/src/Travel.js
@@ -8,7 +8,15 @@ class Travel extends Component {
   render() {
     return (
 <Template menu="Travel">
+<div className="container-fluid">
+<div className="row" id="menurow2">
+Destinations
+</div>
+<div className="row" id="menurow2">
+Cruises
+</div>
 
+</div>
   </Template>
     );
 
