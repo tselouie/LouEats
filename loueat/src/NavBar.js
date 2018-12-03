715196a84c89391f243e31f5ccc6863a6b634a88
Louie Tse <lttse@myseneca.ca>
2018-12-02 21:40:28 -0500

    travel updates

--- loueat/src/NavBar.js
+++ loueat/src/NavBar.js
@@ -7,8 +7,8 @@ class NavBar extends Component {
 
   render() {
     return (
-      
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top ">
+     
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top " id="navv">
         <div className="container">
           <a className="navbar-brand" href="/"><img id="logo-hover" src="img/Logo.png" alt="logo" style={{height:"50px" , width:"50px"}}/></a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
--- loueat/src/Travel.js
+++ loueat/src/Travel.js
@@ -5,18 +5,87 @@ import Template from './Template';
 
 
 class Travel extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleMouseHover = this.handleMouseHover.bind(this);
+    this.state = {
+      isHovering: false,
+    };
+  }
+  handleMouseHover() {
+    this.setState(this.toggleHoverState);
+  }
+
+  toggleHoverState(state) {
+    return {
+      isHovering: !state.isHovering,
+    };
+  }
+
+  
   render() {
     return (
 <Template menu="Travel">
-<div className="container-fluid">
-<div className="row" id="menurow2">
-Destinations
-</div>
-<div className="row" id="menurow2">
-Cruises
-</div>
 
-</div>
+<div className="container-fluid"style={{ padding: 0 }}>
+<h2 id="continentAsia" ><img className="img-fluid" src="img/largecrane.ico" alt="crane"style={{ height: "25px", width: "25px"}}/>Asia</h2>
+<div className="row"id="asia1" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}><a href="google.com">
+<span>
+  <img className="img-fluid"  src="img/cherryblossom.jpg" align="left" alt=""style={{ height: "40px", width: "200px"}}/>
+</span>
+<span id= "zone">Kyoto</span>
+</a>
+ 
+</div> 
+
+<div className="row"id="asia1" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}><a href="google.com">
+<span>
+  <img className="img-fluid"  src="img/cherryblossom.jpg" align="left" alt=""style={{ height: "40px", width: "200px"}}/>
+</span>
+<span id= "zone">Hakone</span>
+ </a>
+</div> 
+
+
+<h2 id="continentEU" >Europe<img className="img-fluid" src="img/EU.ico" alt="euflag"style={{ height: "25px", width: "25px"}}/></h2>
+<div className="row"id="asia1" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}><a href="google.com">
+<span>
+  <img className="img-fluid"  src="img/cherryblossom.jpg" align="left" alt=""style={{ height: "40px", width: "200px"}}/>
+</span>
+<span id= "zone">Kyoto</span>
+ </a>
+</div> 
+
+<div className="row"id="asia1"onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}><a href="google.com">
+<span>
+  <img className="img-fluid"  src="img/cherryblossom.jpg" align="left" alt=""style={{ height: "40px", width: "200px"}}/>
+</span>
+<span id= "zone">Hakone</span>
+ </a>
+</div> 
+
+<h2 id="continentNA" >North America<img className="img-fluid" src="img/NA.jpg"style={{ height: "30px", width: "35px"}}/></h2>
+<div className="row"id="asia1" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}><a href="google.com">
+<span>
+  <img className="img-fluid"  src="img/cherryblossom.jpg" align="left" alt=""style={{ height: "40px", width: "200px"}}/>
+</span>
+<span id= "zone">Kyoto</span>
+ </a>
+</div> 
+
+<div className="row"id="asia1" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}><a href="google.com">
+<span>
+  <img className="img-fluid"  src="img/cherryblossom.jpg" align="left" alt=""style={{ height: "40px", width: "200px"}}/>
+</span>
+<span id= "zone">Hakone</span>
+ </a>
+</div> 
+
+ </div> {/*container-fluid */}
+
+
+
   </Template>
     );
 
